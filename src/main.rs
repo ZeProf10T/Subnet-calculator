9e2cb520b277178225cb977ee080c705ec921d44
Léo Huteau <huteau890@gmail.com>
2020-03-29 14:02:44 +0200

    Add IPv4 test

--- src/main.rs
+++ src/main.rs
@@ -269,3 +269,169 @@ fn main() {
 
 
 
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn basic_ipv4_network_address() {
+        assert_eq!(
+            IPv4::address::network(&Ipv4Addr::new(192, 168, 5, 35), &Ipv4Addr::new(255, 255, 255, 0)),
+            Ipv4Addr::new(192, 168, 5, 0)
+        );
+    }
+
+    #[test]
+    fn complex_ipv4_network_address() {
+        assert_eq!(
+            IPv4::address::network(&Ipv4Addr::new(10,0,35,26), &Ipv4Addr::new(255,255,224,0)),
+            Ipv4Addr::new(10,0,32,0)
+        )
+    }
+
+    #[test]
+    fn basic_ipv4_wildcard_address() {
+        assert_eq!(
+            IPv4::address::wildcard(&Ipv4Addr::new(255,255,255,0)),
+            Ipv4Addr::new(0,0,0,255)
+        )
+    }
+
+    #[test]
+    fn complex_ipv4_wildcard_address() {
+        assert_eq!(
+            IPv4::address::wildcard(&Ipv4Addr::new(255,255,192,0)),
+            Ipv4Addr::new(0,0,63,255)
+        )
+    }
+
+    #[test]
+    fn basic_ipv4_broadcast_address() {
+        assert_eq!(
+            IPv4::address::broadcast(&Ipv4Addr::new(192,168,0,5), &Ipv4Addr::new(255,255,255,0)),
+            Ipv4Addr::new(192,168,0,255)
+        )
+    }
+
+    #[test]
+    fn complex_ipv4_broadcast_address() {
+        assert_eq!(
+            IPv4::address::broadcast(&Ipv4Addr::new(10,0,35,27), &Ipv4Addr::new(255,255,192,0)),
+            Ipv4Addr::new(10,0,63,255)
+        )
+    }
+
+    #[test]
+    fn basic_ipv4_first_address() {
+        assert_eq!(
+            IPv4::address::first(&Ipv4Addr::new(192,168,0,0)),
+            Ipv4Addr::new(192,168,0,1)
+        )
+    }
+
+    #[test]
+    fn basic_ipv4_last_address() {
+        assert_eq!(
+            IPv4::address::last(&Ipv4Addr::new(192,168,0,255)),
+            Ipv4Addr::new(192,168,0,254)
+        )
+    }
+
+    #[test]
+    fn basic_ipv4_subnet_count() {
+        assert_eq!(
+            IPv4::subnet::count(&Ipv4Addr::new(0,0,1,255), &Ipv4Addr::new(0,0,0,255)),
+            2
+        )
+    }
+
+    #[test]
+    fn complex_ipv4_subnet_count() {
+        assert_eq!(
+            IPv4::subnet::count(&Ipv4Addr::new(0,0,15,255), &Ipv4Addr::new(0,0,0,255)),
+            16
+        )
+
+
+    }
+
+    #[test]
+    fn basic_ipv4_mask_to_cidr() {
+        assert_eq!(
+            IPv4::utils::mask_to_cidr(&Ipv4Addr::new(255,255,255,0)),
+            24
+        )
+    }
+
+    #[test]
+    fn complex_ipv4_mask_to_cidr() {
+        assert_eq!(
+            IPv4::utils::mask_to_cidr(&Ipv4Addr::new(255,255,252,0)),
+            22
+        )
+    }
+
+    #[test]
+    fn basic_ipv4_cidr_to_mask() {
+        assert_eq!(
+            IPv4::utils::cidr_to_mask(16),
+            Ipv4Addr::new(255,255,0,0)
+        )
+    }
+
+    #[test]
+    fn complex_ipv4_cidr_to_mask() {
+        assert_eq!(
+            IPv4::utils::cidr_to_mask(18),
+            Ipv4Addr::new(255,255,192,0)
+        )
+    }
+
+    #[test]
+    fn basic_ipv4_vec_to_num() {
+        assert_eq!(
+            IPv4::utils::vec_to_num([255,255,0,0]),
+            0b11111111_11111111_00000000_00000000
+        )
+    }
+
+    #[test]
+    fn complex_ipv4_vec_to_num() {
+        assert_eq!(
+            IPv4::utils::vec_to_num([255,192,0,0]),
+            0b11111111_11000000_00000000_00000000
+        )
+    }
+
+    #[test]
+    fn basic_ipv4_num_to_vec() {
+        assert_eq!(
+            IPv4::utils::num_to_vec(0b11111111_11111111_00000000_00000000),
+            [255,255,0,0,]
+        )
+    }
+
+    #[test]
+    fn complex_ipv4_num_to_vec() {
+        assert_eq!(
+            IPv4::utils::num_to_vec(0b11111111_11000000_00000000_00000000),
+            [255,192,0,0,]
+        )
+    }
+
+    #[test]
+    fn basic_ipv4_host_count() {
+        assert_eq!(
+            IPv4::utils::host_count(&Ipv4Addr::new(0,0,0,255)),
+            254
+        )
+    }
+
+    #[test]
+    fn complex_ipv4_host_count() {
+        assert_eq!(
+            IPv4::utils::host_count(&Ipv4Addr::new(0,0,7,255)),
+            2046
+        )
+    }
+}
